8de2733ff7829eae45987a47cb3ee65279e2327f
Skytlz <Skytlzdev@gmail.com>
2018-03-30 00:31:34 -0500

    updated

--- Assests/script.js
+++ Assests/script.js
@@ -1,14 +1,42 @@
-
-function randomText(){
+/*function SkytlzgetTimeRemaining(endtime) {
 	"use strict";
-	var x = Math.floor(Math.random()*101);
-	if(x<5){
-		document.getElementById("header").innerHTML="9u32iojh4";
-		document.getElementById("title").innerHTML=x;
-		
-	}else{
-		document.getElementById("header").innerHTML="Coming Soon!";
-		document.getElementById("title").innerHTML=x;
-	}
+  var SkytlzT= Date.parse(endtime) - Date.parse(new Date());
+  var Skytlzseconds = Math.floor((SkytlzT % (1000*60))/1000);
+  var Skytlzminutes = Math.floor((SkytlzT %(1000 * 60 * 60))/(1000 * 60));
+  var Skytlzhours = Math.floor((SkytlzT % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var Skytlzdays = Math.floor(SkytlzT / (1000 * 60 * 60 * 24));
+  return {
+    'total': SkytlzT,
+	'days': Skytlzdays,
+    'hours': Skytlzhours,
+    'minutes': Skytlzminutes,
+    'seconds': Skytlzseconds
+  };
 }
-randomText();
+
+function SkytlzinitializeClock(id, endtime) {
+	"use strict";
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function SkytlzupdateClock() {
+    var SkytlzTIME = SkytlzgetTimeRemaining(endtime);
+	daysSpan.innerHTML = ('0' + SkytlzTIME.days).slice(-2);
+    hoursSpan.innerHTML = ('0' + SkytlzTIME.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + SkytlzTIME.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + SkytlzTIME.seconds).slice(-2);
+
+    if (SkytlzTIME.total <= 0) {
+      clearInterval(Skytlztimeinterval);
+    }
+  }
+
+  SkytlzupdateClock();
+  var Skytlztimeinterval = setInterval(SkytlzupdateClock, 1000);
+}
+
+var SkytlzRelease = "2018-05-30T23:00:00Z";
+SkytlzinitializeClock('Skytlzclockdiv', SkytlzRelease);*/
