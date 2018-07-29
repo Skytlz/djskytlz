8f8992f345b7139ceaa7f3719c865031ba6b54bd
Skytlz <Skytlzdev@gmail.com>
2018-07-28 20:42:47 -0500

    the void

---
+++ spacespecies/the-void/purple.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+document.getElementById("purple").addEventListenerListener("Click", purple);
+function purple(){
+	"use strict";
+	document.getElementById("purple").setAttribute.href="/Collections/";
+
+}
+purple();
